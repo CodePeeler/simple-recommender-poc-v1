eae78739ce0b771d9c9e4847abb60bbfdc03b2f1
Simon <dynamowebdesign@gmail.com>
2018-01-16 20:06:01 +0000

    Added some more data source beans

--- src/main/java/com/simon/recommender/MovieController.java
+++ src/main/java/com/simon/recommender/MovieController.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Logger;
 
-import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,40 +23,46 @@ public class MovieController {
 	protected Logger logger = Logger.getLogger(MovieController.class.getName());	
 	
 	@Autowired
-	Connection simpleDAO;
+	Connection simpleConn;
 	
 	@Autowired
-	BasicDataSource dataSourcePool;
+	Connection pooledConn;
 		
 	@RequestMapping("/movies")
-	public Movie movies() throws SQLException {		
+	public Movie movies() {
+		String title = "NO RESULTS";
+	    String reviewers = "NO REVIEWERS";
+	    int numOfReviews = 0;		
+		Movie movie = new Movie(title);
 		
-	/*** Example using simpleDAO ***/
+	/*** Example using a single JDBC connection ***/
 		
 		Statement stmt = null;
 	    ResultSet rs = null;
 		
-	    stmt = simpleDAO.createStatement();
-	    rs = stmt.executeQuery(SQL_QUERY_01);
-
-	    String title = "NO RESULTS";
-	    int numOfReviews = 0;
-	    String reviewers = "NO REVIEWERS";
-	    if (rs.next()) {
-	    	title = rs.getString(1);
-	    	reviewers = rs.getString(2);
-	    	numOfReviews = rs.getInt(3);	    	
-	    }
-	    System.out.println("\nMovie : "+title+"\n Reviewers : "+reviewers+"\n Number of reviews : "+numOfReviews);
-	    Movie movie = new Movie(title);
+	    try {
+			stmt = simpleConn.createStatement();
+			rs = stmt.executeQuery(SQL_QUERY_01);
+			
+		    if (rs.next()) {
+		    	title = rs.getString(1);
+		    	reviewers = rs.getString(2);
+		    	numOfReviews = rs.getInt(3);	    	
+		    }
+		    System.out.println("\nMovie : "+title
+		    				  +"\nReviewers : "+reviewers
+		    				  +"\nNumber of reviews : "+numOfReviews);
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	    
 	   
-    /*** Example using Connection Pool ***/
+    /*** Example using JDBC Connection Pool ***/   
 	    
-	    Connection conn = null;
 	    PreparedStatement pStmt = null;
 	    try {
-		    conn = dataSourcePool.getConnection();
-		    pStmt = conn.prepareStatement(SQL_QUERY_02);
+		    
+		    pStmt = pooledConn.prepareStatement(SQL_QUERY_02);
 		    ResultSet resultSet = pStmt.executeQuery();
 		    
 		    int v1=0, v2=0, v3=0;		    
@@ -70,10 +75,8 @@ public class MovieController {
 		    	}
 	    	}		    
 	    } catch (SQLException e) {
-	    // do something...not sure what, though!
-	    	System.out.println("Looks like we have a problem "+e.getMessage());
-	    }
-      
+	    	e.printStackTrace();
+	    }      
 		return movie;
 	}	
 	
